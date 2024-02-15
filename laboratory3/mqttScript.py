56617040b193c74f75e9e5bcb4405db988fb6c04
a1yak <alyak.grt@gmail.com>
2024-02-15 11:45:22 +0200

    Laboratory work 3

---
+++ laboratory3/mqttScript.py
@@ -0,0 +1,21 @@
+import paho.mqtt.client as mqtt
+import time
+
+def connect_broker(broker_address, client_name):
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, client_name)
+    client.connect(broker_address)
+    time.sleep(1)
+    client.loop_start()
+    return client
+
+if __name__ == '__main__':
+    server = ("broker.hivemq.com")
+    client_name = "lab3client"
+    client = connect_broker(server, client_name)
+    try:
+        while True:
+            message = input("Enter message: ")
+            client.publish("temperature", message)
+    except KeyboardInterrupt:
+        client.disconnect()
+        client.loop_stop()
